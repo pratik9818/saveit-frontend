7ee90dbb81a4f91ad94db501548525c5184b0af7
pratik9818 <pratiksingh7105@gmail.com>
2025-01-29 22:01:15 +0530

    adding back btn in fragment page

--- src/component/LogoutButton.tsx
+++ src/component/LogoutButton.tsx
@@ -59,7 +59,7 @@ export default function LogoutButton() {
     <div className="sm:mr-4">
       <button
         onClick={handleLogout}
-        className="w-full md:w-auto bg-red-500 text-white px-4 py-2 rounded-lg shadow-md hover:bg-red-600 transition duration-300"
+        className="w-full md:w-auto bg-red-500 text-white px-4 py-2  rounded-lg shadow-md hover:bg-red-600 transition duration-300"
       >
         {userLoginState ? "Logout" : "Login"}
       </button>
--- src/utils/Icons.tsx
+++ src/utils/Icons.tsx
@@ -9,6 +9,7 @@ const icons = {
   FileIcon: () => <img className="w-[28px] h-[28px] mx-2" src="/assets/file.png" alt="file" />,
   CopyIcon: () => <img className="w-[20px] h-[20px] mx-2 mt-[4px] cursor-copy" src="/assets/copy.png" alt="copy" />,
   LogoIcon:()=> <img onClick={() => location.reload()} className="w-[70px] h-[35px] md:w-[80px] md:h-[35px] mr-4 cursor-pointer" src="/assets/logo.png" alt="Saveit.tech" />,
+  backIcon:()=> <img className="w-[33px] h-[30px] cursor-pointer mt-1" src="/assets/backicon.png" alt="back" />,
 };
 
 export default icons;
